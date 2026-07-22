44926de245cb163f34b27a74c6107f2e4073f279
unknown <george2066@yandex.ru>
2026-07-22 14:37:05 +0300

    condition

--- condition/src/Main.java
+++ condition/src/Main.java
@@ -80,4 +80,116 @@ void main() {
             System.out.println(one + " больше всех");
         }
     }
+
+
+    char color = 'g';
+    switch (color) {
+        case 'r':
+            System.out.println("Красный! Ехать нельзя!");
+            break;
+        case 'y':
+            System.out.println("Желтый. Внимание.");
+            break;
+        case 'g':
+            System.out.println("Зеленый! Проезд разрешен.");
+            break;
+        case 'l':
+            System.out.println("Поворот налево");
+            break;
+        default:
+            System.out.println("Светофор сломался");
+    }
+
+    age = 6;
+    switch (age) {
+        case 18:
+            System.out.println("С окончанием школы!");
+            break;
+        case 21:
+            System.out.println("Теперь можно пить алкоголь");
+            break;
+        case 7:
+            System.out.println("Иди в школу");
+            break;
+        default:
+            System.out.println("Пока сказать нечего");
+    }
+
+    byte clientOS = 1;
+    int clientDeviceYear = 2016;
+
+    if (clientOS == 1 && clientDeviceYear > 2015) {
+        System.out.println("Установите версию приложения для Android по ссылке");
+    } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+        System.out.println("Установите облегченную версию приложения для Android по ссылке");
+    } else if (clientDeviceYear <= 2015) {
+        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+    } else {
+        System.out.println("Установите версию приложения для iOS по ссылке");
+    }
+
+    int year = 1600;
+
+    if (year <= 1584) {
+        System.out.println("Год должен быть больше, чем 1584");
+    } else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        System.out.println(year + " год является високосным");
+    } else {
+        System.out.println(year + " год не является високосным");
+    }
+
+    byte deliveryDistance = 95;
+
+    if (deliveryDistance <= 20) {
+        System.out.println("Доставка займёт примерно сутки.");
+    } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+        System.out.println("Даставка займёт примерно 2е суток.");
+    } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+        System.out.println("Даставка займёт примерно 3е суток.");
+    } else {
+        System.out.println("Доставки нет.");
+    }
+
+    byte monthNumber = 12;
+
+    switch (monthNumber) {
+        case 1:
+            System.out.println("Зима");
+            break;
+        case 2:
+            System.out.println("Зима");
+            break;
+        case 3:
+            System.out.println("Весна");
+            break;
+        case 4:
+            System.out.println("Весна");
+            break;
+        case 5:
+            System.out.println("Весна");
+            break;
+        case 6:
+            System.out.println("Лето");
+            break;
+        case 7:
+            System.out.println("Лето");
+            break;
+        case 8:
+            System.out.println("Лето");
+            break;
+        case 9:
+            System.out.println("Осень");
+            break;
+        case 10:
+            System.out.println("Осень");
+            break;
+        case 11:
+            System.out.println("Осень");
+            break;
+        case 12:
+            System.out.println("Зима");
+            break;
+        default:
+            System.out.println("Номер месяца должен быть в пределях от 1го до 12ти.");
+    }
 }
